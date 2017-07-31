a39536654114c983dc6aadf4845a1f01daff83b9
Ljx <314041831@qq.com>
2017-07-31 10:46:16 +0800

    Chapter1 finished

---
+++ chapter1/src/main/java/org/smart4j/HelloServlet.java
@@ -0,0 +1,22 @@
+package org.smart4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dataFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
---
+++ chapter1/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ljx
+  Date: 2017/7/31
+  Time: 9:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<h1>Hello!</h1>
+
+<h2>当前时间： ${requestScope.currentTime}</h2>
+</body>
+</html>
