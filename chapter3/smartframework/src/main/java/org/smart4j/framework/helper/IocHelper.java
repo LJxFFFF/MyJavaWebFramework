09bda3de8c5f20115edb2b2d1a31ce5c2f43f2c2
Ljx <314041831@qq.com>
2017-08-16 15:53:53 +0800

    实现依赖注入功能

---
+++ chapter3/smartframework/src/main/java/org/smart4j/framework/helper/IocHelper.java
@@ -0,0 +1,36 @@
+package org.smart4j.framework.helper;
+
+import org.smart4j.framework.annotation.Inject;
+import org.smart4j.framework.util.ArrayUtil;
+import org.smart4j.framework.util.CollectionUtil;
+import org.smart4j.framework.util.ReflectionUtil;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public class IocHelper {
+
+    static {
+        //获取所有Bean类与Bean实例之间的映射关系
+        Map<Class<?>, Object> beanMap = BeanHelper.getBeanMap();
+        if (CollectionUtil.isNotEmpty(beanMap)) {
+            for (Map.Entry<Class<?>, Object> beanEntry : beanMap.entrySet()) {
+                Class<?> beanClass = beanEntry.getKey();    //获取Bean类
+                Object beanInstance = beanEntry.getValue(); //获取Bean实例
+                Field[] beanFields = beanClass.getFields(); //获取Bean类定义的所有成员变量
+                if (ArrayUtil.isNotEmpty(beanFields)) {
+                    for (Field beanField : beanFields) {
+                        if (beanField.isAnnotationPresent(Inject.class)) {//判断当前BeanField中是否带有Inject注解
+                            Class<?> beanFieldClass = beanField.getType();
+                            Object beanFieldInstance = beanMap.get(beanFieldClass);
+                            if (beanFieldInstance != null) {
+                                //通过反射初始化BeanField的值
+                                ReflectionUtil.setField(beanInstance, beanField, beanFieldInstance);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ chapter3/smartframework/src/main/java/org/smart4j/framework/util/ArrayUtil.java
@@ -0,0 +1,20 @@
+package org.smart4j.framework.util;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public final class ArrayUtil {
+
+    /**
+     * 判断数组是否非空
+     */
+    public static boolean isNotEmpty(Object[] array) {
+        return !ArrayUtils.isEmpty(array);
+    }
+
+    /**
+     * 判断数组是否为空
+     */
+    public static boolean isEmpty(Object[] array) {
+        return ArrayUtils.isEmpty(array);
+    }
+}
